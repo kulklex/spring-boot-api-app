54f246279793ed595b29b360f7921c50d54cbcd4
kulklex <kulklex@gmail.com>
2023-04-15 18:47:41 +0100

    creating GET request for postgresql data

--- src/main/java/com/kulklex/Main.java
+++ src/main/java/com/kulklex/Main.java
@@ -4,19 +4,23 @@ package com.kulklex;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
     public static void main (String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/")
-    public String greetings() {
-        return "Hello World testing";
+    private CustomerRepository customerRepository;
+
+    @GetMapping
+    public List<Customer> getCustomers(){
+        return  customerRepository.findAll();
     }
 
     @GetMapping("/greet")
