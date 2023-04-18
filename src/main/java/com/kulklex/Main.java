be6667818e1a1010ff61c08e50b634cfb96945e1
kulklex <kulklex@gmail.com>
2023-04-18 21:24:55 +0100

    Adding other controllers for  crud operations

--- src/main/java/com/kulklex/Main.java
+++ src/main/java/com/kulklex/Main.java
@@ -3,10 +3,10 @@ package com.kulklex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController
@@ -23,6 +23,39 @@ public class Main {
         return  customerRepository.findAll();
     }
 
+    record NewCustomerRequest(
+            String name,
+            String email,
+            Integer age
+    ) {}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+        customerRepository.save(customer);
+    }
+
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable Integer id) {
+        customerRepository.deleteById(id);
+    }
+
+    @PatchMapping("{customerId}")
+    public void updateCustomer(@PathVariable Integer id, NewCustomerRequest newCustomer) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        if (customer.isPresent()) {
+            customer.get().setName(newCustomer.name());
+            customer.get().setEmail(newCustomer.email());
+            customer.get().setAge(newCustomer.age());
+        }
+    }
+
+
+
     @GetMapping("/greet")
     public GreetResponse greet() {
         GreetResponse response =  new GreetResponse(
@@ -34,6 +67,6 @@ public class Main {
         return response;
     }
 
-    record Person (String name, int age, double savings){};
+    record Person (String name, int age, double savings){}
     record GreetResponse (String greet, List<String> FavProgLang, Person person){}
 }
